5cc9a6616ce91472fcf5d7e1a7b44f6b3dbd9977
EC2 Default User <ec2-user@ip-172-31-29-179.us-east-2.compute.internal>
2019-03-03 22:52:22 +0000

    HW3 complete

--- rottenpotatoes/app/controllers/movies_controller.rb
+++ rottenpotatoes/app/controllers/movies_controller.rb
@@ -1,7 +1,7 @@
 class MoviesController < ApplicationController
   
   def movie_params
-    params.require(:movie).permit(:title, :rating, :description, :release_date)
+    params.require(:movie).permit(:title, :rating, :description, :release_date, :director)
   end
 
   def show
@@ -11,6 +11,7 @@ class MoviesController < ApplicationController
   end
 
   def index
+    puts "#{params}"
     sort = params[:sort] || session[:sort]
     case sort
     when 'title'
@@ -48,6 +49,7 @@ class MoviesController < ApplicationController
   end
 
   def update
+    puts "#{params}"
     @movie = Movie.find params[:id]
     @movie.update_attributes!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully updated."
@@ -59,6 +61,17 @@ class MoviesController < ApplicationController
     @movie.destroy
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
+  end
+  
+  def similar
+    puts "#{params}"
+    @movie = Movie.find params[:id]
+    if @movie.director.blank?
+      flash[:notice] = "'#{@movie.title}' has no director info"
+      redirect_to movies_path
+    else
+      @movies = Movie.samedirector(@movie.director)
+    end
   end
 
 end
--- rottenpotatoes/app/models/movie.rb
+++ rottenpotatoes/app/models/movie.rb
@@ -2,4 +2,8 @@ class Movie < ActiveRecord::Base
   def self.all_ratings
     %w(G PG PG-13 NC-17 R)
   end
+  
+  def self.samedirector director
+    Movie.where(:director => director)
+  end
 end
